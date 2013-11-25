6d30b2b6de8770cae57bdbd0c5ec8a0e553aa16f
Laxman Dewangan <ldewangan@nvidia.com>
2013-11-25 19:39:47 +0530

    irq: Enable all irqs unconditionally in irq_resume
    commit ac01810c9d2814238f08a227062e66a35a0e1ea2 upstream.
    
    When the system enters suspend, it disables all interrupts in
    suspend_device_irqs(), including the interrupts marked EARLY_RESUME.
    
    On the resume side things are different. The EARLY_RESUME interrupts
    are reenabled in sys_core_ops->resume and the non EARLY_RESUME
    interrupts are reenabled in the normal system resume path.
    
    When suspend_noirq() failed or suspend is aborted for any other
    reason, we might omit the resume side call to sys_core_ops->resume()
    and therefor the interrupts marked EARLY_RESUME are not reenabled and
    stay disabled forever.
    
    To solve this, enable all irqs unconditionally in irq_resume()
    regardless whether interrupts marked EARLY_RESUMEhave been already
    enabled or not.
    
    This might try to reenable already enabled interrupts in the non
    failure case, but the only affected platform is XEN and it has been
    confirmed that it does not cause any side effects.
    
    [ tglx: Massaged changelog. ]
    
    Signed-off-by: Laxman Dewangan <ldewangan@nvidia.com>
    Acked-by-and-tested-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Acked-by: Heiko Stuebner <heiko@sntech.de>
    Reviewed-by: Pavel Machek <pavel@ucw.cz>
    Cc: <ian.campbell@citrix.com>
    Cc: <rjw@rjwysocki.net>
    Cc: <len.brown@intel.com>
    Cc: <gregkh@linuxfoundation.org>
    Link: http://lkml.kernel.org/r/1385388587-16442-1-git-send-email-ldewangan@nvidia.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- kernel/irq/pm.c
+++ kernel/irq/pm.c
@@ -50,7 +50,7 @@ static void resume_irqs(bool want_early)
 		bool is_early = desc->action &&
 			desc->action->flags & IRQF_EARLY_RESUME;
 
-		if (is_early != want_early)
+		if (!is_early && want_early)
 			continue;
 
 #ifdef CONFIG_PM_DEBUG
